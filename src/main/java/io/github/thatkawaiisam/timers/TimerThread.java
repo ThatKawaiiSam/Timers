8b923e13822a74dd94658ee9be8042c76f7b504d
ThatKawaiiSam <sam.sergi@outlook.com>
2019-08-07 20:40:25 +1000

    Fixed some logic inconsistencies and commented around.

--- src/main/java/io/github/thatkawaiisam/timers/TimerThread.java
+++ src/main/java/io/github/thatkawaiisam/timers/TimerThread.java
@@ -27,24 +27,24 @@ public class TimerThread extends Thread {
     }
 
     private void doTimerLogic() {
-        Instant now = Instant.now();
-        if (timerManager == null
-                || now == null) {
+        final Instant now = Instant.now();
+        if (timerManager == null || now == null) {
             return;
         }
         if (timerManager.getBeginTime() == null) {
-            timerManager.setBeginTime(Instant.now());
+            timerManager.setBeginTime(now);
         }
         timerManager.setDeltaTime(Duration.between(timerManager.getBeginTime(), now));
 
+        // Check if tick should be called.
         if (timerManager.getDeltaTime().getSeconds() > 0) {
             timerManager.getTimers().stream()
                     .filter(timer -> !timer.isPaused())
                     .forEach(Timer::tick);
-
             timerManager.setBeginTime(now);
         }
 
+        // Check if timer is complete
         timerManager.getTimers().stream()
                 .filter(timer -> !timer.isPaused())
                 .filter(Timer::isComplete)
